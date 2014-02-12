839f3e1d73ceb7091c31f0269ff6a0770bd7cc22
Paul Hethmon <paul.hethmon@clareitysecurity.com>
2014-02-11 20:44:40 -0500

    Increment version/date
    Increment version number and date of login servlet build.

--- mcb-project/src/main/java/edu/internet2/middleware/assurance/mcb/authn/provider/MCBLoginServlet.java
+++ mcb-project/src/main/java/edu/internet2/middleware/assurance/mcb/authn/provider/MCBLoginServlet.java
@@ -57,7 +57,7 @@ import edu.internet2.middleware.shibboleth.idp.util.HttpServletHelper;
  */
 public class MCBLoginServlet extends HttpServlet {
 
-	public static final String VERSION = "1.0.0 -- 2014-01-05";
+	public static final String VERSION = "1.0.1 -- 2014-02-11";
 	/**
 	 * Serial UID 
 	 */
