337aa40ccb4fdef4d12d41ec4080656c316eafb2
David Langenberg <davel@uchicago.edu>
2014-02-15 14:06:41 -0700

    Version bump

--- mcb-project/src/main/java/edu/internet2/middleware/assurance/mcb/authn/provider/MCBLoginServlet.java
+++ mcb-project/src/main/java/edu/internet2/middleware/assurance/mcb/authn/provider/MCBLoginServlet.java
@@ -57,7 +57,7 @@ import edu.internet2.middleware.shibboleth.idp.util.HttpServletHelper;
  */
 public class MCBLoginServlet extends HttpServlet {
 
-	public static final String VERSION = "1.0.1 -- 2014-02-11";
+	public static final String VERSION = "1.1.0";
 	/**
 	 * Serial UID 
 	 */
